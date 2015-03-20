11e25138696b3e70e67a64a62d8a04b98d0c8961
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2015-03-19 20:47:47 -0400

    Add docstrings

--- blocks/log.py
+++ blocks/log.py
@@ -3,11 +3,24 @@ from collections import Mapping, OrderedDict
 from numbers import Integral
 from operator import methodcaller
 
+from picklable_itertools import imap
 from pymongo import ASCENDING, MongoClient
-from six.moves import map
 
 
 class DefaultOrderedDict(OrderedDict):
+    r"""A ordered dictionary that supports default values.
+
+    Parameters
+    ----------
+    default_factor : callable, optional
+        A callable that returns the default value when a key is not found.
+        If not given, a KeyError will be raised instead.
+    \*args
+        Interpreted as ``dict``.
+    \*\*kwargs
+        Interpreted as ``dict``.
+
+    """
     def __init__(self, default_factory=None, *args, **kwargs):
         if default_factory is not None and not callable(default_factory):
             raise TypeError('first argument must be callable')
@@ -26,6 +39,33 @@ class DefaultOrderedDict(OrderedDict):
 
 
 class TrainingLog(Mapping):
+    """The log of training progress.
+
+    A training log stores the training timeline, statistics and other
+    auxiliary information. Information is represented as nested dictionary
+    of iteration, key values.
+
+    In addition to the set of records of the training progress, a training
+    log has a status object whose attributes describe the current state of
+    training e.g. whether training has started, whether termination has
+    been requested, etc.
+
+    Parameters
+    ----------
+    backend : {'default', 'mongo', 'sqlite'}
+        The log can be stored in a variety of backends. The `default`
+        backend stores the values in a nested dictionary (a
+        :class:`DefaultOrderedDict` to be precise). This is a simple and
+        fast option, but the log is difficult to access for analysis during
+        training, and can only be saved as a pickled Python object.
+
+        The `mongo` option uses a MongoDB database server to store the
+        logs. The `sqlite` value will use a local SQLite file to store the
+        results. Both of these backends will allow you to analyze results
+        easily even during training, and will allow you to save multiple
+        experiments to the same database.
+
+    """
     def __init__(self, backend='default', **kwargs):
         self.status = TrainingStatus()
         if backend == 'default':
@@ -56,6 +96,25 @@ class TrainingLog(Mapping):
 
 
 class TrainingStatus(Mapping):
+    """The status of the training process.
+
+    By default this contains two keys: `iterations_done` and `epochs_done`.
+
+    Parameters
+    ----------
+    exclude : list, optional
+        A list of status keys (strings) that should be excluded when
+        iterating over the status items. This can be useful when you want
+        to store a particular tidbit of information that should be
+        accessible to e.g. extensions, but don't want this to show up in
+        the log at every time step.
+
+    Attributes
+    ----------
+    exclude : list
+        See the parameters section.
+
+    """
     def __init__(self, exclude=None):
         self._status = {'iterations_done': 0, 'epochs_done': 0}
         self.exclude = [] if exclude is None else exclude
@@ -74,6 +133,7 @@ class TrainingStatus(Mapping):
 
 
 class MongoTrainingLog(Mapping):
+    """A training log stored in a MongoDB database."""
     def __init__(self):
         self.client = MongoClient()
         self.db = self.client.blocks_log
@@ -83,15 +143,31 @@ class MongoTrainingLog(Mapping):
         return MongoEntry(self, key)
 
     def __iter__(self):
-        return map(methodcaller('pop', '_id'),
-                   self.entries.find(projection=['_id'],
-                                     sort=[('_id', ASCENDING)]))
+        return imap(methodcaller('pop', '_id'),
+                    self.entries.find(projection=['_id'],
+                                      sort=[('_id', ASCENDING)]))
 
     def __len__(self):
         return self.entries.count()
 
 
 class MongoEntry(Mapping):
+    """A single entry in the MongoDB-based log.
+
+    Parameters
+    ----------
+    log : :class:`MongoTrainingLog`
+        The log this entry belongs to.
+    key : int
+        The key (``iterations_done``) that this entry belongs to.
+
+    Notes
+    -----
+    Entries are their own objects so that we can read lazily from the
+    MongoDB database, so if the user just wants to write new values, it
+    won't retrieve the old values.
+
+    """
     def __init__(self, log, key):
         self.log = log
         self.key = key
